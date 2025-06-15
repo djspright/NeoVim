77f6ebddf8ec09b6926ea6a33255eddff4914217
djspright <gdlnolgs@gmail.com>
2025-06-15 17:34:09 +0900

    change divider

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -52,8 +52,8 @@ return {
       return {
         options = {
           theme = dracula_theme,
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
           globalstatus = true,
           disabled_filetypes = { statusline = { "dashboard", "alpha" } },
         },
